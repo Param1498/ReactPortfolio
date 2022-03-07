bbb33fe9b41d98e03524f7692b67efe0027b634e
Param1498 <43669502+Param1498@users.noreply.github.com>
2022-03-07 21:16:24 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,20 @@ import { tawkToLoadScripts } from './Tawkchat'
 
 
 const Header = () => {
+  const Header = () => {
+  useEffect(() => {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src='https://embed.tawk.to/622601861ffac05b1d7d5a28/1fti8vtsi';
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
   return (
     <>
     <header>
